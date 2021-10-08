b2c7bb99e114eef19862b71c9e45b0e49134f3ea
Ziyad Shoeky <mrbrain@msn.com>
2021-10-08 18:19:01 +0200

    Update song_details_screen.dart

--- lib/screens/song_details_screen.dart
+++ lib/screens/song_details_screen.dart
@@ -60,35 +60,3 @@ class TrackDetailsUI extends StatelessWidget {
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       home: new Scaffold(
-//         appBar: new AppBar(
-//           title: new Text('UrlLauchner'),
-//         ),
-//         body: new Center(
-//           child: new RichText(
-//             text: new TextSpan(
-//               children: [
-//                 new TextSpan(
-//                   text: 'This is no Link, ',
-//                   style: new TextStyle(color: Colors.black),
-//                 ),
-//                 new TextSpan(
-//                   text: 'but this is',
-//                   style: new TextStyle(color: Colors.blue),
-//                   recognizer: new TapGestureRecognizer()
-//                     ..onTap = () { launch('https://docs.flutter.io/flutter/services/UrlLauncher-class.html');
-//                     },
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
